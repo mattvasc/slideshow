b16900866866d18ba902e32430bc61c0a77f13f9
Mateus Silva Vasconcelos <mateus.vasconcelos@opus-software.com.br>
2018-09-04 10:00:14 -0300

    TRABALHEM EM BRANCH PROPRIA MEUS AMIGOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { EditPresentationComponent } from './edit-presentation/edit-presentation.component';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
