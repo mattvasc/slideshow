0d36cdcc967341e57faae48b5d032e340f1f7812
Mateus Silva Vasconcelos <mateus.vasconcelos@opus-software.com.br>
2018-09-04 09:28:59 -0300

    new method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ThumbnailsMenuComponent } from './thumbnails-menu/thumbnails-menu.component';
 import { UserComponent } from './user/user.component';
 import { EditPresentationComponent } from './edit-presentation/edit-presentation.component';
+import { LoginComponent } from './login/login.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { ManagePresentationsComponent } from './manage-presentations/manage-presentations.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ThumbnailsMenuComponent,
 		UserComponent,
-		EditPresentationComponent
+		EditPresentationComponent,
+		LoginComponent,
+		WorkspaceComponent,
+		ManagePresentationsComponent
 	],
 	imports: [
 		BrowserModule,
