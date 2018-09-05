1aae0e3bf24821cf0bd77d852ce8e47b5aa84cdb
Lucas Kimura <lucas.kimura@opus-software.com.br>
2018-09-05 14:53:50 -0300

    alteracoes workspace

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -19,5 +19,23 @@ export class WorkspaceComponent implements OnInit {
 	}
 	render() {
 		// this.slide.render();
-	}
+  }
+  fireEventEditar(e) {
+    //Gera bloco de texto editavel com as mesmas dimensões e posição que o <p> por cima para editar ou gerar modal no meio da tela
+    console.log(e.clientX);
+    console.log(e.clientY);
+  }
+  fireEventMover(e) {
+    //pegar posição do mouse global e verificar se a posição é uma das bordas do elemento
+    //caso seja chamar função de redimensionar(?) - extra (?)
+    console.log(e.clientX);
+    console.log(e.screenX);
+    console.log(e.clientY);
+    console.log(e.screenY);
+  }
+  //funções notáveis
+//ao clicar fora da pagina do slide, se há slide em modo de edição, renderizar e sair da edição
+//ao clicar em algum elemento, chamar função
+//-> caso tenha clicado dentro, entrar modo edição daquele elemento
+//-> caso clicou na borda, permitir arrastar
 }
