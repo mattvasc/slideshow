dd5406cd28dcb2ab0ff4e8de30709e39283bc6c2
Mateus Silva Vasconcelos <mateus.vasconcelos@opus-software.com.br>
2018-09-17 10:39:51 -0300

    removed Merge conflict marker

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-
-=======
 import { User } from './user';
->>>>>>> dev
 @Injectable({
 	providedIn: 'root'
 })
@@ -11,13 +7,8 @@ export class ApiService {
 
 	constructor() { }
 
-<<<<<<< HEAD
-	login(username: String, password: String): boolean {
-		if (password === '123') {
-=======
 	login(user: User): boolean {
 		if (user.password === '123') {
->>>>>>> dev
 			return true;
 		} else {
 			return false;
